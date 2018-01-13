cb67a82c28ae29f30a5e3ab353883e9905a17f0b
Johannah Sims <jjrsims@gmail.com>
2018-01-13 21:20:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,29 @@
 def turn(board)
   puts "Please enter 1-9:"
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board,index,char="X")
+  board[index] = char
+end
+
+def valid_move?(board,position)
+  position.to_i.between?(0,8) && !position_taken?(board, position)
+end
+
+def position_taken?(board,position)
+  return false if [" ", "", nil].include?(board[position])
+  return true if ["X", "O"].include?(board[position])
+  raise "#{board[position]} is not a valid move"
+end
